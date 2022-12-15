7fc616bc3352408d42dd622d7d1cc2b9e3acbc35
Siyanbola Ajibola <85687155+harjeeyborlah@users.noreply.github.com>
2022-12-15 11:07:14 +0100

    tailwindcssnetninja

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}","index.html"],
+  theme: {
+    extend: {
+      colors:{
+        primary:"#FF6363",
+        secondary:{
+          100:"#E2E2D5",
+          200:"#888883",
+        }
+      }
+    },
+  },
+  plugins: [],
+}
